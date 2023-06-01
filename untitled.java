f27dc22428e01fc6b0cde613ba9c52cdc9083ee9
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-06-01 13:28:45 +0530

    Name Type with star decoration in only one code

---
+++ untitled.java
@@ -0,0 +1,9 @@
+// Java Litteral
+
+// Write My Name With Some Decoration
+
+class Example {
+    public static void main(String args[]) {
+        System.out.print("* \t\t\t\t*\n\t*\t\t*\n*\t\t\t\t*\n\t\t*\n*\t\t\t\t*\n\n*\t\t\t\t* \n\n\n\n\n*\t*\t*\t*\t*\n\n\t\t*\n\n\t\t*\n\n\t\t*\n\n*\t*\t*\t*\t* \n\n\n\n*\t\t\t\t*\n\n*\t\t\t\t*\n\n*\t*\t*\t*\t*\n\n*\t\t\t\t*\n\n*\t\t\t\t*\n\n\n\n*\t*\t*\t*\t*\n\n\t\t*\n\n\t\t*\n\n\t\t*\n\n\t\t*\n\n*\t*\t*\t*\t*\n\n\n\n*\t*\t*\n\t\t\t*\n\n*\t\t\t     *\n\n\t\t\t*\n*\t*\t*\n\t*\n*\n\t\t*\n*\n\t\t\t*\n*\t\t\t   * \n\n\n\n\t\t\t*\n\t\t*\t\t*\n\t     *\t\t\t   *\n\t   *\t\t\t     *\n\t  *\t\t\t      *\n\t *  *  *  *  *  *  *  *  *  *  *\n\t *\t\t\t       *\n\t *\t\t\t       *\n\t *\t\t\t       *\n\t *\t\t\t       *\n\t *\t\t\t       *\n\t *\t\t\t       *\n\t *\t\t\t       * ");
+    }
+}
